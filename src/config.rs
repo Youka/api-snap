5cadb6acd3ddfb079a9c163f5f325b1fcd583599
Youka <spanknebel.borken@t-online.de>
2024-01-23 11:41:13 +0100

    reduce config redundancy

--- src/config.rs
+++ src/config.rs
@@ -5,28 +5,29 @@ pub const APP_NAME: &str = env!("CARGO_PKG_NAME");
 pub const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 // Static
-pub const OPENAPI_PORT_ANNOTATION: &str = "openapi/port";
-pub const OPENAPI_PATH_ANNOTATION: &str = "openapi/path";
+macro_rules! third_party_dir { () => { "third-party" } }
+pub(crate) use third_party_dir;
+
+macro_rules! port_annotation_postfix { () => { "port" } }
+macro_rules! path_annotation_postfix { () => { "path" } }
+macro_rules! openapi_annotation_prefix { () => { "openapi/" } }
+macro_rules! asyncapi_annotation_prefix { () => { "asyncapi/" } }
+macro_rules! graphql_annotation_prefix { () => { "graphql/" } }
+
+pub const OPENAPI_PORT_ANNOTATION: &str = concat!(openapi_annotation_prefix!(), port_annotation_postfix!());
+pub const OPENAPI_PATH_ANNOTATION: &str = concat!(openapi_annotation_prefix!(), path_annotation_postfix!());
 pub const DEFAULT_OPENAPI_PATH: &str = "/openapi";
-pub const ASYNCAPI_PORT_ANNOTATION: &str = "asyncapi/port";
-pub const ASYNCAPI_PATH_ANNOTATION: &str = "asyncapi/path";
+pub const ASYNCAPI_PORT_ANNOTATION: &str = concat!(asyncapi_annotation_prefix!(), port_annotation_postfix!());
+pub const ASYNCAPI_PATH_ANNOTATION: &str = concat!(asyncapi_annotation_prefix!(), path_annotation_postfix!());
 pub const DEFAULT_ASYNCAPI_PATH: &str = "/asyncapi";
-pub const GRAPHQL_PORT_ANNOTATION: &str = "graphql/port";
-pub const GRAPHQL_PATH_ANNOTATION: &str = "graphql/path";
+pub const GRAPHQL_PORT_ANNOTATION: &str = concat!(graphql_annotation_prefix!(), port_annotation_postfix!());
+pub const GRAPHQL_PATH_ANNOTATION: &str = concat!(graphql_annotation_prefix!(), path_annotation_postfix!());
 pub const DEFAULT_GRAPHQL_PATH: &str = "/graphql";
 pub const DEFAULT_API_PORT: u16 = 80;
 
-macro_rules! env_var_prefix { () => { env!("CARGO_PKG_NAME").to_uppercase().replace("-", "_") + "_" } }
-macro_rules! app_namespace { () => { env!("CARGO_PKG_NAME").to_lowercase().replace("-", "") } }
-macro_rules! third_party_dir { () => { "third-party" } }
-
-pub(crate) use {
-    app_namespace,
-    env_var_prefix,
-    third_party_dir
-};
-
 // Dynamic
+macro_rules! env_var_prefix { () => { env!("CARGO_PKG_NAME").to_uppercase().replace("-", "_") + "_" } }
+
 pub fn get_address() -> String {
     env::env_var_as_string(&(env_var_prefix!() + "ADDRESS")).unwrap_or("127.0.0.1".to_owned())
 }
--- src/endpoints/metrics.rs
+++ src/endpoints/metrics.rs
@@ -9,7 +9,7 @@ use crate::{
 };
 
 pub fn build_prometheus_metrics_middleware() -> Result<PrometheusMetrics, Box<dyn std::error::Error + Send + Sync>> {
-    PrometheusMetricsBuilder::new(&config::app_namespace!())
+    PrometheusMetricsBuilder::new(&config::APP_NAME.to_lowercase().replace('-', ""))
         .const_labels(labels_to_map(&[
             ("app_name", config::APP_NAME),
             ("app_version", config::APP_VERSION)
