3c116223e006b33e649145fbf52a46a50f876880
Youka <spanknebel.borken@t-online.de>
2024-01-23 12:15:16 +0100

    define test strategy

--- src/utils/collections.rs
+++ src/utils/collections.rs
@@ -5,3 +5,22 @@ pub fn labels_to_map(labels: &[(&str, &str)]) -> HashMap<String,String> {
         .map(|label| (label.0.to_owned(), label.1.to_owned()))
         .collect()
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn labels_to_map_empty() {
+        assert!(labels_to_map(&[]).is_empty())
+    }
+
+    #[test]
+    fn labels_to_map_two_entries() {
+        assert_eq!(
+            labels_to_map(&[("foo", "bar"), ("hello", "world")]),
+            [("hello".to_owned(), "world".to_owned()), ("foo".to_owned(), "bar".to_owned())].into_iter().collect()
+        )
+    }
+}
