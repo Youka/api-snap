fbac104cef3105bd7b40623091b6c7cda522007a
Youka <spanknebel.borken@t-online.de>
2024-01-26 21:47:05 +0100

    add string utils tests

--- src/utils/string.rs
+++ src/utils/string.rs
@@ -27,3 +27,37 @@ pub fn process_template(template: &str, vars: &[(&str,&str)]) -> String {
         |output, (key, value)| output.replace(&format!("{{{{{}}}}}", key), value)
     )
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn logged_parse_ok() {
+        const TEST_VALUE: i32 = 1337;
+        assert_eq!(Some(TEST_VALUE), TEST_VALUE.to_string().logged_parse("leet"));
+    }
+
+    #[test]
+    fn logged_parse_fail() {
+        assert_eq!(None, "".logged_parse::<u8>("empty"));
+    }
+
+    #[test]
+    fn process_template_foo_bar() {
+        assert_eq!(
+            "Hello 'foo', or should i call you 'bar'?",
+            process_template("Hello '{{placeholder1}}', or should i call you '{{placeholder2}}'?", &[("placeholder1", "foo"), ("placeholder2", "bar")])
+        );
+    }
+
+    #[test]
+    fn process_template_miss_placeholder() {
+        const TEMPLATE: &str = "Hello {{test}}!";
+        assert_eq!(
+            TEMPLATE,
+            process_template(TEMPLATE, &[])
+        );
+    }
+}
