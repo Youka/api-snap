2954f44062382e8eba777e95fcac214b7ba7e4d0
Youka <spanknebel.borken@t-online.de>
2024-01-27 13:30:45 +0100

    add http utils tests

--- src/utils/http.rs
+++ src/utils/http.rs
@@ -26,3 +26,65 @@ pub async fn http_get(url: &str) -> AnyResult<Bytes> {
         Err(err) => bail!("Http get request failed: {} => {}", url, err)
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread::spawn;
+    use actix_web::{
+        rt::System,
+        test::TestRequest,
+        web::get,
+        App,
+        HttpServer
+    };
+
+    #[test]
+    fn extract_http_url_short_example() {
+        assert_eq!(
+            "http://example.com/",
+            extract_http_url(TestRequest::default()
+                .uri("http://example.com")
+                .to_http_request()
+            )
+        );
+    }
+
+    #[test]
+    fn extract_http_url_long_example() {
+        assert_eq!(
+            "http://example.com/test",
+            extract_http_url(TestRequest::default()
+                .uri("http://example.com/test?foo=bar")
+                .to_http_request()
+            )
+        );
+    }
+
+    #[test]
+    fn http_get_example() {
+        const TEST_BIND: (&str, u16) = ("127.0.0.1", 8989);
+        const EXAMPLE_BODY: &str = "example";
+
+        let server = HttpServer::new(||
+            App::new().route("/", get().to(|| async { EXAMPLE_BODY }))
+        ).bind(TEST_BIND).unwrap().run();
+        let server_handle = server.handle();
+
+        let server_thread = spawn(|| {
+            System::new().block_on(server).unwrap();
+        });
+
+        System::new().block_on(async {
+            let response = http_get(&format!("http://{}:{}", TEST_BIND.0, TEST_BIND.1)).await.unwrap();
+            assert_eq!(
+                EXAMPLE_BODY.as_bytes().to_vec(),
+                response.to_vec()
+            );
+
+            server_handle.stop(true).await;
+            server_thread.join().unwrap();
+        });
+    }
+}
