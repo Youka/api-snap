6d7401d413e4f00eaccbb2ee0f5c5c6335609a28
Youka <spanknebel.borken@t-online.de>
2024-02-03 00:25:07 +0100

    avoid redownload third-party

--- build.rs
+++ build.rs
@@ -4,10 +4,12 @@ use std::{
     ffi::OsStr,
     fs::{
         create_dir_all,
-        remove_dir_all,
         write as write_file
     },
-    path::PathBuf
+    path::{
+        Path,
+        PathBuf
+    }
 };
 use cargo_metadata::{
     Metadata,
@@ -60,82 +62,87 @@ fn get_ui_versions(metadata: &Metadata) -> (&str, &str, &str) {
 }
 
 fn download_asyncapi_react(asyncapi_react_version: &str) {
-    create_dir_all_new(THIRD_PARTY_ASYNCAPI_REACT_DIR);
-
-    download_file(
-        &format!("https://unpkg.com/@asyncapi/react-component@{}/styles/default.min.css", asyncapi_react_version),
-        PathBuf::from(THIRD_PARTY_ASYNCAPI_REACT_DIR).join("default.min.css")
-    );
-    download_file(
-        &format!("https://unpkg.com/@asyncapi/react-component@{}/browser/standalone/index.js", asyncapi_react_version),
-        PathBuf::from(THIRD_PARTY_ASYNCAPI_REACT_DIR).join("index.js")
-    );
-    download_file(
-        "https://raw.githubusercontent.com/asyncapi/website/master/public/favicon.ico",
-        PathBuf::from(THIRD_PARTY_ASYNCAPI_REACT_DIR).join("favicon.ico")
-    );
+    if create_dir_new(THIRD_PARTY_ASYNCAPI_REACT_DIR) {
+        download_file(
+            &format!("https://unpkg.com/@asyncapi/react-component@{}/styles/default.min.css", asyncapi_react_version),
+            PathBuf::from(THIRD_PARTY_ASYNCAPI_REACT_DIR).join("default.min.css")
+        );
+        download_file(
+            &format!("https://unpkg.com/@asyncapi/react-component@{}/browser/standalone/index.js", asyncapi_react_version),
+            PathBuf::from(THIRD_PARTY_ASYNCAPI_REACT_DIR).join("index.js")
+        );
+        download_file(
+            "https://raw.githubusercontent.com/asyncapi/website/master/public/favicon.ico",
+            PathBuf::from(THIRD_PARTY_ASYNCAPI_REACT_DIR).join("favicon.ico")
+        );
+    }
 }
 
 fn download_swagger_ui(swagger_ui_version: &str) {
-    create_dir_all_new(THIRD_PARTY_SWAGGER_UI_DIR);
-
-    let swagger_ui_archive = web_get(&format!("https://github.com/swagger-api/swagger-ui/archive/refs/tags/v{}.tar.gz", swagger_ui_version))
-        .call().unwrap()
-        .into_reader();
-    let unpack_dir = PathBuf::from(THIRD_PARTY_SWAGGER_UI_DIR);
-    for entry in Archive::new(GzDecoder::new(swagger_ui_archive)).entries().unwrap() {
-        let mut file = entry.unwrap();
-        let path = file.path().unwrap();
-        if path.parent().and_then(|p| p.file_name()) == Some(OsStr::new("dist")) && path.extension() != Some(OsStr::new("map")) {
-            file.unpack(unpack_dir.join(path.file_name().unwrap())).unwrap();
+    if create_dir_new(THIRD_PARTY_SWAGGER_UI_DIR) {
+        let swagger_ui_archive = web_get(&format!("https://github.com/swagger-api/swagger-ui/archive/refs/tags/v{}.tar.gz", swagger_ui_version))
+            .call().unwrap()
+            .into_reader();
+        let unpack_dir = PathBuf::from(THIRD_PARTY_SWAGGER_UI_DIR);
+        for entry in Archive::new(GzDecoder::new(swagger_ui_archive)).entries().unwrap() {
+            let mut file = entry.unwrap();
+            let path = file.path().unwrap();
+            if path.parent().and_then(|p| p.file_name()) == Some(OsStr::new("dist")) && path.extension() != Some(OsStr::new("map")) {
+                file.unpack(unpack_dir.join(path.file_name().unwrap())).unwrap();
+            }
         }
     }
 }
 
 fn download_prism_graphql(prism_graphql_version: &str) {
-    create_dir_all_new(THIRD_PARTY_PRISM_GRAPHQL_DIR);
-
-    download_file(
-        &format!("https://unpkg.com/prismjs@{}/themes/prism.min.css", prism_graphql_version),
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism.min.css")
-    );
-    download_file(
-        &format!("https://unpkg.com/prismjs@{}/components/prism-core.min.js", prism_graphql_version),
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-core.min.js")
-    );
-    download_file(
-        &format!("https://unpkg.com/prismjs@{}/components/prism-graphql.min.js", prism_graphql_version),
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-graphql.min.js")
-    );
-    download_file(
-        &format!("https://unpkg.com/prismjs@{}/plugins/line-numbers/prism-line-numbers.min.css", prism_graphql_version),
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-line-numbers.min.css")
-    );
-    download_file(
-        &format!("https://unpkg.com/prismjs@{}/plugins/line-numbers/prism-line-numbers.min.js", prism_graphql_version),
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-line-numbers.min.js")
-    );
-    download_file(
-        &format!("https://unpkg.com/prismjs@{}/plugins/toolbar/prism-toolbar.min.css", prism_graphql_version),
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-toolbar.min.css")
-    );
-    download_file(
-        &format!("https://unpkg.com/prismjs@{}/plugins/toolbar/prism-toolbar.min.js", prism_graphql_version),
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-toolbar.min.js")
-    );
-    download_file(
-        &format!("https://unpkg.com/prismjs@{}/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js", prism_graphql_version),
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-copy-to-clipboard.min.js")
-    );
-    download_file(
-        "https://raw.githubusercontent.com/graphql/graphql.github.io/source/static/favicon.ico",
-        PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("favicon.ico")
-    );
+    if create_dir_new(THIRD_PARTY_PRISM_GRAPHQL_DIR) {
+        download_file(
+            &format!("https://unpkg.com/prismjs@{}/themes/prism.min.css", prism_graphql_version),
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism.min.css")
+        );
+        download_file(
+            &format!("https://unpkg.com/prismjs@{}/components/prism-core.min.js", prism_graphql_version),
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-core.min.js")
+        );
+        download_file(
+            &format!("https://unpkg.com/prismjs@{}/components/prism-graphql.min.js", prism_graphql_version),
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-graphql.min.js")
+        );
+        download_file(
+            &format!("https://unpkg.com/prismjs@{}/plugins/line-numbers/prism-line-numbers.min.css", prism_graphql_version),
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-line-numbers.min.css")
+        );
+        download_file(
+            &format!("https://unpkg.com/prismjs@{}/plugins/line-numbers/prism-line-numbers.min.js", prism_graphql_version),
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-line-numbers.min.js")
+        );
+        download_file(
+            &format!("https://unpkg.com/prismjs@{}/plugins/toolbar/prism-toolbar.min.css", prism_graphql_version),
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-toolbar.min.css")
+        );
+        download_file(
+            &format!("https://unpkg.com/prismjs@{}/plugins/toolbar/prism-toolbar.min.js", prism_graphql_version),
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-toolbar.min.js")
+        );
+        download_file(
+            &format!("https://unpkg.com/prismjs@{}/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js", prism_graphql_version),
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("prism-copy-to-clipboard.min.js")
+        );
+        download_file(
+            "https://raw.githubusercontent.com/graphql/graphql.github.io/source/static/favicon.ico",
+            PathBuf::from(THIRD_PARTY_PRISM_GRAPHQL_DIR).join("favicon.ico")
+        );
+    }
 }
 
-fn create_dir_all_new(path: &str) {
-    remove_dir_all(path).unwrap_or(());
-    create_dir_all(path).unwrap();
+fn create_dir_new(path: &str) -> bool {
+    if Path::new(path).is_dir() {
+        println!("cargo:warning=Directory already exists! Delete for downloading updates: {}", path);
+        false
+    } else {
+        create_dir_all(path).unwrap();
+        true
+    }
 }
 
 fn download_file(url: &str, filepath: PathBuf) {
